90c7b5aee3e75f5ba9470ecf9acfb2cdfbad2467
LucaPiccolo7 <119038765+LucaPiccolo7@users.noreply.github.com>
2023-06-11 16:56:45 +0200

    Add files via upload

---
+++ jdk-20-scripts.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk-20/bin/jar 1
+
+sudo update-alternatives --install /usr/bin/jarsigner jarsigner /usr/lib/jvm/jdk-20/bin/jarsigner 1
+
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-20/bin/java 1
+
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk-20/bin/javac 1
+
+sudo update-alternatives --install /usr/bin/javadoc javadoc /usr/lib/jvm/jdk-20/bin/javadoc 1
+
+sudo update-alternatives --install /usr/bin/javap javap /usr/lib/jvm/jdk-20/bin/javap 1
+
+sudo update-alternatives --install /usr/bin/jcmd jcmd /usr/lib/jvm/jdk-20/bin/jcmd 1
+
+sudo update-alternatives --install /usr/bin/jconsole jconsole /usr/lib/jvm/jdk-20/bin/jconsole 1
+
+sudo update-alternatives --install /usr/bin/jdb jdb /usr/lib/jvm/jdk-20/bin/jdb 1
+
+sudo update-alternatives --install /usr/bin/jdeprscan jdeprscan /usr/lib/jvm/jdk-20/bin/jdeprscan 1
+
+sudo update-alternatives --install /usr/bin/jdeps jdeps /usr/lib/jvm/jdk-20/bin/jdeps 1
+
+sudo update-alternatives --install /usr/bin/jfr jfr /usr/lib/jvm/jdk-20/bin/jfr 1
+
+sudo update-alternatives --install /usr/bin/jhsdb jhsdb /usr/lib/jvm/jdk-20/bin/jhsdb 1
+
+sudo update-alternatives --install /usr/bin/jimage jimage /usr/lib/jvm/jdk-20/bin/jimage 1
+
+sudo update-alternatives --install /usr/bin/jinfo jinfo /usr/lib/jvm/jdk-20/bin/jinfo 1
+
+sudo update-alternatives --install /usr/bin/jlink jlink /usr/lib/jvm/jdk-20/bin/jlink 1
+
+sudo update-alternatives --install /usr/bin/jmap jmap /usr/lib/jvm/jdk-20/bin/jmap 1
+
+sudo update-alternatives --install /usr/bin/jmod jmod /usr/lib/jvm/jdk-20/bin/jmod 1
+
+sudo update-alternatives --install /usr/bin/jpackage jpackage /usr/lib/jvm/jdk-20/bin/jpackage 1
+
+sudo update-alternatives --install /usr/bin/jps jps /usr/lib/jvm/jdk-20/bin/jps 1
+
+sudo update-alternatives --install /usr/bin/jrunscript jrunscript /usr/lib/jvm/jdk-20/bin/jrunscript 1
+
+sudo update-alternatives --install /usr/bin/jshell jshell /usr/lib/jvm/jdk-20/bin/jshell 1
+
+sudo update-alternatives --install /usr/bin/jstack jstack /usr/lib/jvm/jdk-20/bin/jstack 1
+
+sudo update-alternatives --install /usr/bin/jstat jstat /usr/lib/jvm/jdk-20/bin/jstat 1
+
+sudo update-alternatives --install /usr/bin/jstatd jstatd /usr/lib/jvm/jdk-20/bin/jstatd 1
+
+sudo update-alternatives --install /usr/bin/jwebserver jwebserver /usr/lib/jvm/jdk-20/bin/jwebserver 1
+
+sudo update-alternatives --install /usr/bin/keytool keytool /usr/lib/jvm/jdk-20/bin/keytool 1
+
+sudo update-alternatives --install /usr/bin/rmiregistry rmiregistry /usr/lib/jvm/jdk-20/bin/rmiregistry 1
+
+sudo update-alternatives --install /usr/bin/serialver serialver /usr/lib/jvm/jdk-20/bin/serialver 1
+
+sudo update-alternatives --config jar
+sudo update-alternatives --config javap
+sudo update-alternatives --config jdeps
+sudo update-alternatives --config jlink
+sudo update-alternatives --config jrunscript
+sudo update-alternatives --config jwebserver
+sudo update-alternatives --config jarsigner
+sudo update-alternatives --config jcmd
+sudo update-alternatives --config jfr
+sudo update-alternatives --config jmap
+sudo update-alternatives --config jshell
+sudo update-alternatives --config keytool
+sudo update-alternatives --config java
+sudo update-alternatives --config jconsole
+sudo update-alternatives --config jhsdb
+sudo update-alternatives --config jmod
+sudo update-alternatives --config jstack
+sudo update-alternatives --config rmiregistry
+sudo update-alternatives --config javac
+sudo update-alternatives --config jdb
+sudo update-alternatives --config jimage
+sudo update-alternatives --config jpackage
+sudo update-alternatives --config jstat
+sudo update-alternatives --config serialver
+sudo update-alternatives --config javadoc
+sudo update-alternatives --config jdeprscan
+sudo update-alternatives --config jinfo
+sudo update-alternatives --config jps
+sudo update-alternatives --config jstatd
+
